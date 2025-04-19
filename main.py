12c8f8500708b8dc898bede4988e40c4571aa80a
Qinghua <frenchfriesdb@gmail.com>
2025-04-18 19:03:59 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello☹️")
+print("YOLO " * 10)
+
+x = 1
+
+is_Public = True
+
+if is_Public == False:
+    print("This is a private course")
+else:
+    print("This is a public course")
+
+students_count = 1000
+rating = 4.99
+print(students_count)
+print(type(students_count))
+print(type(x))
